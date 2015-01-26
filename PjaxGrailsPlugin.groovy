c3e15fab61954bc8b2282a1ee2e632cb8019933b
Dong Weiqiang <weiqiang@gmail.com>
2015-01-27 01:47:45 +0800

    update doc src, and v 0.0.7

--- PjaxGrailsPlugin.groovy
+++ PjaxGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class PjaxGrailsPlugin {
-    def version = "0.0.6"
+    def version = "0.0.7"
     def grailsVersion = "2.0 > *"
 
     def title = "Grails Pjax Plugin"
@@ -7,7 +7,7 @@ class PjaxGrailsPlugin {
     def authorEmail = "dongwqs@sina.com"
     def description = 'Grails Pjax Plugin: more easy pjax in Grails.See more https://github.com/dongwq/grails-pjax'
 
-    def documentation = "http://grails.org/plugin/pjax"
+    def documentation = "https://github.com/dongwq/grails-pjax"
 
     def license = "APACHE"
     def issueManagement = [system: "GitHub", url: 'https://github.com/dongwq/grails-pjax/issues']
